51b5022963f9842b516bb7c89ab7e3e40606d968
colaf <colaf@colaf.net>
2023-09-26 17:53:23 +0900

    almost done

--- app/views/index.py
+++ app/views/index.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 bp = Blueprint('index', __name__, url_prefix='/')
 
-@bp.route('/')
+@bp.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method =='GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        return render_template('authentication.html')
+    else:
+        return "Hello world"
